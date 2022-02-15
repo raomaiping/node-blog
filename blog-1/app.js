0804a1cc5e249e60ab6509d493adc26a507fde0f
rmp <2582395486@qq.com>
2022-02-15 11:16:07 +0800

    初始化路由

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,14 +1,33 @@
+const handleBlogRouter = require("./src/router/blog");
+const handleUserRouter = require("./src/router/user");
+
 const serverHandle = (req, res) => {
   //设置返回格式 JSON
   res.setHeader("Content-type", "application/json");
+  //获取path
+  const url = req.url;
+  req.path = url.split("?")[0];
 
-  const resData = {
-    name: "双越100",
-    site: "imooc",
-    env: process.env.NODE_ENV,
-  };
+  //处理blog路由
+  const blogData = handleBlogRouter(req, res);
+  if (blogData) {
+    res.end(JSON.stringify(blogData));
+    return;
+  }
 
-  res.end(JSON.stringify(resData));
+  //处理user路由
+  const userData = handleUserRouter(req, res);
+  if (userData) {
+    res.end(JSON.stringify(userData));
+    return;
+  }
+
+  //未命中路由,返回404
+  res.writeHead(404, { "Content-type": "text/plain" });
+  res.write("404 Not Found\n");
+  res.end();
 };
 
 module.exports = serverHandle;
+
+// process.env.NODE_ENV,
