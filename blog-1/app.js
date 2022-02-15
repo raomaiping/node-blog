aa9e97024eb9f2c05e551620494a5f4cd9bd1046
rmp <2582395486@qq.com>
2022-02-15 10:36:59 +0800

    搭建开发环境

---
+++ blog-1/app.js
@@ -0,0 +1,14 @@
+const serverHandle = (req, res) => {
+  //设置返回格式 JSON
+  res.setHeader("Content-type", "application/json");
+
+  const resData = {
+    name: "双越100",
+    site: "imooc",
+    env: process.env.NODE_ENV,
+  };
+
+  res.end(JSON.stringify(resData));
+};
+
+module.exports = serverHandle;
