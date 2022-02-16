d053c32a657da5a876920d2243c6389eaeb36a16
rmp <2582395486@qq.com>
2022-02-16 16:44:57 +0800

    session 登录

--- blog-1/app.js
+++ blog-1/app.js
@@ -1,6 +1,14 @@
 const querystring = require("querystring");
 const handleBlogRouter = require("./src/router/blog");
 const handleUserRouter = require("./src/router/user");
+//获取 cookie 的过期时间
+const getCookieExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+  return d.toGMTString();
+};
+// session 数据
+const SESSION_DATA = {};
 
 // 处理 post data
 const getPostData = (req) => {
@@ -39,6 +47,30 @@ const serverHandle = (req, res) => {
   //解析 query
   req.query = querystring.parse(url.split("?")[1]);
 
+  //解析cookie
+  req.cookie = {};
+  const cookieStr = req.headers.cookie || "";
+  cookieStr.split(";").forEach((item) => {
+    if (!item) return;
+    const arr = item.split("=");
+    const key = arr[0].trim();
+    const val = arr[1].trim();
+    req.cookie[key] = val;
+  });
+
+  // 解析 session
+  let needSetCookie = false;
+  let userId = req.cookie.userid;
+  if (userId) {
+    if (!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {};
+    }
+  } else {
+    needSetCookie = true;
+    userId = `${Date.now()}_${Math.random()}`;
+    SESSION_DATA[userId] = {};
+  }
+  req.session = SESSION_DATA[userId];
   // 处理 post data
   getPostData(req).then((postData) => {
     req.body = postData;
@@ -46,6 +78,12 @@ const serverHandle = (req, res) => {
     const blogResult = handleBlogRouter(req, res);
     if (blogResult) {
       blogResult.then((blogData) => {
+        if (needSetCookie) {
+          res.setHeader(
+            "Set-Cookie",
+            `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`
+          );
+        }
         res.end(JSON.stringify(blogData));
       });
       return;
@@ -54,6 +92,12 @@ const serverHandle = (req, res) => {
     const userResult = handleUserRouter(req, res);
     if (userResult) {
       userResult.then((userData) => {
+        if (needSetCookie) {
+          res.setHeader(
+            "Set-Cookie",
+            `userid=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`
+          );
+        }
         res.end(JSON.stringify(userData));
       });
       return;
