ab7f8da56f4395e889d36469eed473794191f1ed
rmp <2582395486@qq.com>
2022-02-16 13:53:16 +0800

    api 对接mysql-博客更新和删除

--- blog-1/src/controller/blog.js
+++ blog-1/src/controller/blog.js
@@ -42,12 +42,28 @@ const newBlog = (blogData = {}) => {
 const updateBlog = (id, blogData = {}) => {
   // id 就是要更新博客的id
   //blogData 是一个博客对象，包含title content 属性
-  return true;
+  const { title, content } = blogData;
+
+  const sql = `
+      update blogs set title='${title}', content='${content}' where id=${id}
+  `;
+  return exec(sql).then((updateData) => {
+    if (updateData.affectedRows > 0) {
+      return true;
+    }
+    return false;
+  });
 };
 
-const delBlog = (id) => {
+const delBlog = (id, author) => {
   // id 就是要删除博客的id
-  return true;
+  const sql = `delete from blogs where id='${id}' and author='${author}';`;
+  return exec(sql).then((delData) => {
+    if (delData.affectedRows > 0) {
+      return true;
+    }
+    return false;
+  });
 };
 module.exports = {
   getList,
--- blog-1/src/router/blog.js
+++ blog-1/src/router/blog.js
@@ -37,21 +37,26 @@ const handleBlogRouter = (req, res) => {
   //更新一篇博客
   if (method === "POST" && req.path === "/api/blog/update") {
     const result = updateBlog(id, req.body);
-    if (result) {
-      return new SuccessModel();
-    } else {
-      return new ErrorModel("更新博客失败");
-    }
+    return result.then((val) => {
+      if (val) {
+        return new SuccessModel();
+      } else {
+        return new ErrorModel("更新博客失败");
+      }
+    });
   }
 
   //删除一篇博客
   if (method === "POST" && req.path === "/api/blog/del") {
-    const result = delBlog(id);
-    if (result) {
-      return new SuccessModel();
-    } else {
-      return new ErrorModel("删除博客失败");
-    }
+    const author = "zhangsan"; //假数据,待开发登录时再改成真实数据
+    const result = delBlog(id, author);
+    return result.then((val) => {
+      if (val) {
+        return new SuccessModel();
+      } else {
+        return new ErrorModel("删除博客失败");
+      }
+    });
   }
 };
 
