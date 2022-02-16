2c33608685e866becc3beea8a7fc2df30ca08e04
rmp <2582395486@qq.com>
2022-02-16 18:03:07 +0800

    完成server端登录代码

--- blog-1/src/router/blog.js
+++ blog-1/src/router/blog.js
@@ -6,6 +6,14 @@ const {
   delBlog,
 } = require("../controller/blog");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
+
+// 统一的登录验证函数
+const loginCheck = (req) => {
+  if (!req.session.username) {
+    return Promise.resolve(new ErrorModel("尚未登录"));
+  }
+};
+
 const handleBlogRouter = (req, res) => {
   const method = req.method; //GET POST
   const id = req.query.id;
@@ -28,7 +36,13 @@ const handleBlogRouter = (req, res) => {
 
   //新建一篇博客
   if (method === "POST" && req.path === "/api/blog/new") {
-    req.body.author = "zhangsan"; //假数据,待开发登录时再改成真实数据
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      //未登录
+      return loginCheck;
+    }
+
+    req.body.author = req.session.username;
     const result = newBlog(req.body);
     return result.then((data) => {
       return new SuccessModel(data);
@@ -36,6 +50,11 @@ const handleBlogRouter = (req, res) => {
   }
   //更新一篇博客
   if (method === "POST" && req.path === "/api/blog/update") {
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      //未登录
+      return loginCheck;
+    }
     const result = updateBlog(id, req.body);
     return result.then((val) => {
       if (val) {
@@ -48,7 +67,12 @@ const handleBlogRouter = (req, res) => {
 
   //删除一篇博客
   if (method === "POST" && req.path === "/api/blog/del") {
-    const author = "zhangsan"; //假数据,待开发登录时再改成真实数据
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      //未登录
+      return loginCheck;
+    }
+    const author = req.session.username;
     const result = delBlog(id, author);
     return result.then((val) => {
       if (val) {
